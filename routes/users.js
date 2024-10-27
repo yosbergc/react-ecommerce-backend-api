6c0aeb3693db50c65e94b7b2e1d8749d8d06b8d1
Yosber Guerrero <yosbergc@gmail.com>
2024-10-27 10:40:54 -0400

    Added post request

--- routes/users.js
+++ routes/users.js
@@ -11,8 +11,18 @@ router.get('/', async (req, res) => {
 router.get('/:id', (req, res) => {
 
 })
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+    const { id, firstName, lastName, email, password } = req.body
 
+    if (!id || !firstName || !lastName || !email || !password) {
+        return res.status(400).send('Bad Request')
+    }
+
+    const client = await Connection();
+    const response = await client.query(`INSERT INTO users (id, firstName, lastName, email, password) VALUES ($1,$2, $3, $4, $5)`, [id, firstName, lastName, email, password])
+    res.json({
+        response: response.rows
+    })
 })
 router.delete('/:id', (req, res) => {
 
