3dfbc6b153c0b4f860c80dba01bcf8954bc5b91b
Yosber Guerrero <yosbergc@gmail.com>
2024-10-27 10:44:28 -0400

    Added bcrypt and try catch

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
 const Connection = require('../libs/postgres')
 const router = express.Router()
 
@@ -13,16 +14,20 @@ router.get('/:id', (req, res) => {
 })
 router.post('/', async (req, res) => {
     const { id, firstName, lastName, email, password } = req.body
-
+    
     if (!id || !firstName || !lastName || !email || !password) {
         return res.status(400).send('Bad Request')
     }
-
-    const client = await Connection();
-    const response = await client.query(`INSERT INTO users (id, firstName, lastName, email, password) VALUES ($1,$2, $3, $4, $5)`, [id, firstName, lastName, email, password])
-    res.json({
-        response: response.rows
+    try {
+        const newPassword = await bcrypt.hash(password, 4);
+        const client = await Connection();
+        const response = await client.query(`INSERT INTO users (id, firstName, lastName, email, password) VALUES ($1,$2, $3, $4, $5)`, [id, firstName, lastName, email, newPassword])
+        res.json({
+            response: response.rows
     })
+    } catch(error) {
+        res.status(400).send('We got one error')
+    }
 })
 router.delete('/:id', (req, res) => {
 
